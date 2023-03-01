f918bb6952bc5db1e5891265403cf89cb226b9d7
tbagaciunas <113136639+tbagaciunas@users.noreply.github.com>
2023-03-01 13:41:05 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,3 +1,9 @@
 $('.message a').click(function(){
    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
+
+
+$('.login-form button').click(function(e) {
+  e.preventDefault(); // prevent the form from submitting normally
+  window.location.href = "index.html"; // redirect to the main page
+});
