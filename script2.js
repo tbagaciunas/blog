8f9152bddf65687c13ac62aad805d145cbcd8a56
tbagaciunas <113136639+tbagaciunas@users.noreply.github.com>
2023-03-01 13:57:13 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -5,5 +5,5 @@ const loginBtn = document.querySelector('.login-form button');
 // Add an event listener to the login button
 loginBtn.addEventListener('click', () => {
   // Load the index.html file
-  window.location.href = './index.html';
+  window.location.href = 'index.html';
 });
